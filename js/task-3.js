beb258d756d12efd54bf681d5cde63081a375131
Zooakva <zooakva2@gmail.com>
2024-02-28 18:38:18 +0200

    a

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,14 @@
-function getElementWidth(content, padding, border) {
-    return (Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2);
+function checkForSpam(message) {
+  // Перевірка на вміст заборонених слів (spam або sale)
+  const lowerCaseMessage = message.toLowerCase();
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// Тестові виклики для перевірки роботи функції
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
